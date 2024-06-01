a2f205f717644f7c62f0650d2ebe055760220051
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-05-31 22:29:50 -0300

    remove: remove códigos gerador pelo projeto padrão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'login_page';
-}
+
+export class AppComponent {}
